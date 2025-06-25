63eee386c128cb8beaaac184c2e3fb78fbc0685b
VladimirBaklan <uladzimirbaklan@gmail.com>
2025-06-26 00:43:23 +0200

    fix external ip

--- test.py
+++ test.py
@@ -1,21 +1,28 @@
-import socket
-import aiohttp
 import asyncio
+import aiohttp
+import socket
 
 class BindToDeviceTCPConnector(aiohttp.TCPConnector):
     def __init__(self, device_name, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.device_name = device_name
+        self.device_name = device_name.encode()
 
-    async def _wrap_create_connection(self, protocol_factory, host, port, **kwargs):
-        def factory():
+    async def _create_connection(self, req, *args, **kwargs):
+        # This method creates the connection socket
+        # We override it to create a socket with SO_BINDTODEVICE set
+
+        loop = self._loop
+
+        def _sock_factory():
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # Bind to interface:
-            sock.setsockopt(socket.SOL_SOCKET, 25, self.device_name.encode())  # 25 is SO_BINDTODEVICE
+            # Bind to device, Linux-only and requires privileges
+            sock.setsockopt(socket.SOL_SOCKET, 25, self.device_name)  # 25 is SO_BINDTODEVICE
             sock.setblocking(False)
             return sock
 
-        return await self._loop.create_connection(protocol_factory, host, port, sock=factory(), **kwargs)
+        kwargs['sock'] = _sock_factory()
+
+        return await super()._create_connection(req, *args, **kwargs)
 
 async def test_request():
     connector = BindToDeviceTCPConnector('enx020054323163')
